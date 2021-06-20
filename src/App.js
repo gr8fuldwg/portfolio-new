b91530baaf06cf5d64aedd6188045f2d909fb1dc
gr8fuldwg <gr8fuldwg@gmail.com>
2021-06-20 17:53:17 -0400

    added typewriter effect to "greeting" section of portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
 import React, { Component } from "react";
-import Card from "react-bootstrap/Card";
 import "./App.css";
 import axios from "axios";
+
+// HTML CSS JSResult Skip Results Iframe
+// EDIT ON
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+// Resources1× 0.5× 0.25×Rerun
 
 // API Variable
 const QUOTE_API = "https://api.quotable.io/random";
@@ -64,19 +126,40 @@ class App extends Component {
           </div>
         </nav>
         <div className="body">
-          <p>
+          <h2 className="greet">
+            Hi, I'm Jon  : ) <br />
+            I build things.
+            
+            {/* CSS Animation TypeWriterEFFECT */}
             <br />
-            Hello.
-            <br /> I'm Jon.
-            <br /> I build things...
-            <br /> Web Pages. Apps. Relationships.
-          </p>
+            <h2
+              className="greet"
+              href=""
+              class="typewrite "
+              data-period="2000"
+              data-type='[ "Web Pages...", "Web Apps...", " Relationships." ]'
+            >
+              <span class="wrap"></span>
+            </h2>
+            <br />
+          </h2>
+
+          {/* <div className="body"> */}
+          {/* <p id="greet">
+            Hi, I'm Jon Josephson.
+            <div id="pitch">
+              <br /> I build things...
+              <br /> Web Pages. Apps. Relationships.
+            </div>
+          </p> */}
+          {/* </div> */}
         </div>
+
         <div id="quote">
           <h3>"{this.state.content}"</h3>
           <h3 className="font-italic">
-            {this.state.author}
             {this.state.hyphen}
+            {this.state.author}
           </h3>
         </div>
       </div>
